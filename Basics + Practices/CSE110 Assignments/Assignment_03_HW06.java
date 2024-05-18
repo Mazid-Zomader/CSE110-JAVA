b45f444f1c715873892d3b249ec83e640c1adb33
Mazid Zomader <159147937+Mazid-Zomader@users.noreply.github.com>
2024-05-19 01:54:47 +0600

    Add files via upload

---
+++ Basics + Practices/CSE110 Assignments/Assignment_03_HW06.java
@@ -0,0 +1,42 @@
+import java.util.Scanner ;
+public class Assignment_03_HW06 {
+  public static void main(String [] args) {
+    Scanner sc = new Scanner(System.in) ;
+    System.out.print("Enter 1st number: ") ;
+    float num1 = sc.nextFloat() ;
+    System.out.print("Enter 2nd number: ") ;
+    float num2 = sc.nextFloat() ;
+    System.out.print("Enter 3rd number: ") ;
+    float num3 = sc.nextFloat() ;
+    if (num1 > num2  && num2 > num3) 
+    {
+      System.out.println("Maximum number: "+ num1) ;
+      System.out.print("Minimum number: "+ num3) ;
+    }
+    else if (num3 > num1 && num1 > num2) 
+    {
+      System.out.println("Maximum number: "+ num3) ;
+      System.out.print("Minimum number: "+ num2) ;
+    }
+    else if ((num1>num3) && (num3> num2 ))
+    {
+      System.out.println("Maximum number: "+ num1) ;
+      System.out.print("Minimum number: "+ num2) ;
+    }
+    else if ((num2>num3) && (num3> num1) )
+    {
+      System.out.println("Maximum number: "+ num2) ;
+      System.out.print("Minimum number: "+ num1) ;
+    }
+    else if ((num3>num2) && (num2> num1) )
+    {
+      System.out.println("Maximum number: "+ num3) ;
+      System.out.print("Minimum number: "+ num1) ;
+    }
+    else if ((num2>num1) && (num1> num3) )
+    {
+      System.out.println("Maximum number: "+ num2) ;
+      System.out.print("Minimum number: "+ num3) ;
+    }
+  }
+}
