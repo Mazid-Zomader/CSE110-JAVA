a7459605ad792be5cc8ae2ed4155ee1a12d8d391
Mazid Zomader <159147937+Mazid-Zomader@users.noreply.github.com>
2024-09-04 19:02:36 +0600

    Create Ans_04.java

---
+++ Assignment Solves/SUMMER 2024 Solves/Assignment 05 Solves/Ans_04.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Ans_04 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Number of test cases: ");
+        int a = sc.nextInt();
+        for (int i = 0; i < a ; i++){
+            int sum = 0 ;
+            System.out.print("Start: ");
+            int start = sc.nextInt();
+            System.out.print("Number of Odd Numbers: ");
+            int odd = sc.nextInt();
+//            boolean flag = false;
+            while (odd > 0){
+                if (start %2 != 0) {
+                    sum += start ;
+                    odd -=1;
+                }
+                start += 1 ;
+            }
+            System.out.println(sum);
+        }
+
+    }
+}
