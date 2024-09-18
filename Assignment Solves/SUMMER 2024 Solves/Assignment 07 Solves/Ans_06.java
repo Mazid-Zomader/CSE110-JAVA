e127c9af6c5a8f1b4fbca8b62a35e6eb35026ae9
Mazid Zomader <159147937+Mazid-Zomader@users.noreply.github.com>
2024-09-18 16:20:58 +0600

    Create Ans_06.java

---
+++ Assignment Solves/SUMMER 2024 Solves/Assignment 07 Solves/Ans_06.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Ans_HW01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the array: ");
+        int length = sc.nextInt();
+        double arr []= new double[length];
+        double sum = 0;
+        double max = Double.NEGATIVE_INFINITY;
+        double min = Double.POSITIVE_INFINITY;
+        int maxIndex = 0;
+        int minIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter a number: ");
+            arr[i] = sc.nextDouble();
+            sum += arr[i];
+            if (arr[i] > max) {
+                max = arr[i];
+                maxIndex = i;
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+                minIndex = i;
+            }
+        }
+        System.out.println("Maximum element: " + max+" found at index " + maxIndex);
+        System.out.println("Minimum element: " + min+" found at index " + minIndex);
+        System.out.println("Summation: " + sum);
+        System.out.format("Average: %.2f" , sum / arr.length);
+    }
+}
