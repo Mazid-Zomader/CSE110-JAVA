e3a54b867d7373f981cbb9bd8b2608f400d0769d
Mazid Zomader <159147937+Mazid-Zomader@users.noreply.github.com>
2024-09-18 16:23:49 +0600

    Update Ans_06.java

--- Assignment Solves/SUMMER 2024 Solves/Assignment 07 Solves/Ans_06.java
+++ Assignment Solves/SUMMER 2024 Solves/Assignment 07 Solves/Ans_06.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ans_HW01 {
+public class Ans_07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the length of the array: ");
